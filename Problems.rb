febfcc470160ae6405b8d7aec5a4829c7a3c83da
Alex Lam <alam.seraph@gmail.com>
2023-05-22 19:57:35 -0700

    climbing stairs

--- Problems.rb
+++ Problems.rb
@@ -4,22 +4,45 @@
 
 # Return the head of the merged linked list.
 
-def merge_two_lists(list1, list2)
-  merged_list = list1 + list2
+# def merge_two_lists(list1, list2)
+#   merged_list = list1 + list2
 
-  p merged_list.sort
-end
-merge_two_lists([1,2,4], [1,3,4])
+#   p merged_list.sort
+# end
+# merge_two_lists([1,2,4], [1,3,4])
 
 
-merged_list = []
-while !list1.empty? && !list2.empty? 
-  if list1.first >= list2.first 
-    merged_list << list1.shift
-  else 
-    merged_list << list2.shift
-  end 
-end
-merged_list += list1 + list2
+# merged_list = []
+# while !list1.empty? && !list2.empty? 
+#   if list1.first >= list2.first 
+#     merged_list << list1.shift
+#   else 
+#     merged_list << list2.shift
+#   end 
+# end
+# merged_list += list1 + list2
 
-p merged_list
+# p merged_list
+
+
+
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# def climb_stairs(n)
+#   p 1 if n == 1 
+#   p 2 if n == 2
+
+#   ways = [0] * (n + 1) 
+#   ways[1] = 1
+#   ways[2] = 2
+
+#   (3..n).each do |x|
+#     ways[x] = ways[x-1] + ways[x-2] 
+#   end
+
+#   p ways[n]
+
+# end
+# climb_stairs(5)
